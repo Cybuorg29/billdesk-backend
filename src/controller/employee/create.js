3b32eee6753fadf8f8b6aa28e361980fc42693fa
Arc Technologies <118550452+ArctechTechnologies@users.noreply.github.com>
2023-07-15 21:17:08 +0530

    .....

--- src/controller/employee/create.js
+++ src/controller/employee/create.js
@@ -6,25 +6,30 @@ exports.createEmployee=async(req,res)=>{
 
     
     try{
+           console.log(req.file?.path)
+          console.log(req.body)
         const { data, token } = req.body;
          if(!data||!token){
 
              return res.status(200).json({ code: 400, message: 'an error occured please try again ' })
             }
-            const { name, adress, salary, balance, image,type,phone } = data
-            if (!name || !adress || !salary || !balance || !image || !token||!phone) {
+            const { name, adress, salary, balance,type,phone } = data
+            if (!name || !adress || !salary || !balance  || !token||!phone) {
                 return res.status(200).json({ code: 400, message: 'form not fully filled ' })
             }
-               const id =  await UserModel.convertToken(token);
+            const id =  await UserModel.convertToken(token);
+            const image = req?.file?.path;
+            if(!image){
+                     return res.status(200).json({ code: 400, message: 'an error occured please try adding image' })
+                    
+                 }
+                 console.log('iamge',image)
             const uploadEmployee = await employeeModel.create({ name, adress, salary, balance, image ,id,type,phone});
             uploadEmployee.save();
-            return res.status(200).json({ code: 200, message:'Employee added sucessfully'})
+            return res.status(200).json({ code: 200, message:'Employee added sucessfully',data:uploadEmployee})
 
-
-        
-        
     }catch(err){
-        return res.status(200).json({code:err.code,message:'en error occured',description:err.message})
+        return res.status(200).json({code:500 ,message:'en error occured',description:err.message})
     }
     
 }
--- src/router/employeeRouter.js
+++ src/router/employeeRouter.js
@@ -2,9 +2,28 @@ const express = require('express')
 const { createEmployee } = require('../controller/employee/create');
 const { getEmployee } = require('../controller/employee/get');
 const router = express.Router()
+const cloudinary = require('cloudinary').v2
+const { CloudinaryStorage } = require('multer-storage-cloudinary')
+const multer = require('multer')
 
 
-router.post('/create/employee',createEmployee);
+cloudinary.config({
+    cloud_name: "dcnu7ucih",
+    api_key: "715421924649762",
+    api_secret: "fih7IsE57u1mTDWqpwCg9VsFhyM"
+})
+
+
+
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: 'employee',
+    },
+});
+
+const upload = multer({ storage: storage })
+router.post('/create/employee',upload.single('image'),createEmployee);
 router.get('/get/employee/:token',getEmployee)
 
 
