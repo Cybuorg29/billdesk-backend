c785d6c537b3f91ec01c89f031d98ccf011c30ad
Arc Technologies <118550452+ArctechTechnologies@users.noreply.github.com>
2023-09-09 12:02:11 +0530

    feat:demo

--- src/controller/connection/get.js
+++ src/controller/connection/get.js
@@ -2,6 +2,28 @@ const profileModel = require("../../model/ProfileModel");
 const connectionModel = require("../../model/connectionModel");
 const UserModel = require("../../model/userModel");
 
+
+
+
+
+let data =[];
+
+ async function  getData(type,id){
+    if(type){
+        res = await profileModel.findOne({id:id});
+        res = await {...res?._doc,role:0};
+        console.log(res)
+        data.push(res)
+    }else{
+        res = await profileModel.findOne({id:id});
+        res = await {...res?._doc,role:1};
+
+        console.log(res)
+        data.push(res)
+    }
+
+}
+
 exports.getConnections = async (req, res) => {
       try {
 
@@ -9,26 +31,14 @@ exports.getConnections = async (req, res) => {
             const { token } = req.params;
             const _id = await UserModel.convertToken(token);
             const Connections = await connectionModel.find({ $or: [{ cid: _id }, { sid: _id }] });
-            let data = []
-               data = await  Connections.map(async(index) => {
-                   let res;
-                    if(index.type){
-                        res = await profileModel.findOne({id:index.cid});
-                        res = await {...res,role:0};
-                    }else{
-                       res = await profileModel.findOne({id:index.sid});
-                       res =  await {...res,role:1};
+             await  Promise.all(Connections.map(async(index)=>{
+                   (index.type)? await getData(index.type,index.sid): await getData(index.type,index.cid)
+               })
+             )
 
-                    }
-                     console.log(res)
-                    if(!res){
-                        console.log('asdasdasd')
-                    }else{
-                        return res
-                    }
-            })
-            console.log('data',data);
-            return res.status(200).json({ code: 200, package: data })
+                console.log(data)
+            
+            return res.status(200).json({ code: 200, package: data});
       } catch (err) {
             console.log(err.message);
             return res.status(200).json({ code: 500, message: 'an error occured', error: err.message })
--- src/router/ConnectionRouter.js
+++ src/router/ConnectionRouter.js
@@ -1,9 +1,7 @@
-
-
 const express = require('express');
-const {addOfflineClient  } = require('../controller/OfflineUser/CreateOfflineUser');
-const { getConnections } = require('../controller/connection/get');
 const router = express.Router();
+const  {getConnections}  = require('../controller/connection/get');
+const { addOfflineClient } = require('../controller/OfflineUser/CreateOfflineUser');
 
 router.post('/user/create/connection',addOfflineClient);
 router.get('/user/get/connectiondata/:token',getConnections);
