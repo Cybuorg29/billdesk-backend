f9fef821be54597bc8ee1b6c0292d8493b8c28d7
Arc Technologies <118550452+ArctechTechnologies@users.noreply.github.com>
2023-12-03 21:29:34 +0530

    ....

--- src/controller/profile.js
+++ src/controller/profile.js
@@ -15,53 +15,8 @@ exports.getUserDataByToken=async(req,res)=>{
             return res.status(200).json({code:100,message:'please set up your profile'})
           }
            const bank = await bankModel.findOne({id:id})
-           const d = new Date()
-          let  day = (d.getDate() - d.getDate() ) +1
-          const greaterMonth = String(d.getMonth() + 1).padStart(2, '0');
-          const Lessmonth = String(d.getMonth() + 0 ).padStart(2, '0');
-          let year = d.getFullYear()
-          var twoDigitYear = year.toString().substr(-2);
-          // const date = `${day}-${month}-${twoDigitYear}`
-           const  greater = `${day}-${Lessmonth}-${twoDigitYear}`
-           const less = `${day}-${greaterMonth}-${twoDigitYear}`
-            console.log(greater,less)
-          const exp = await  expencesModel.find({id:id})
-           const expences = [];
-           exp.map((index)=>{
-              const d = index.date.split('-')
-               const month = d[1];
-                const year = d[2];
-                if(twoDigitYear===year){
-                  //  console.log('year')
-                  if(greaterMonth<=month){
-                      // console.log('month')
-                      if(Lessmonth<=less){
-                          // console.log('index')
-                          expences.push(index)
-                      }
-                  }
-                }
-           })
-             const inc = (await incomeModel.find({id:id}))
-              let income = [];
-             inc.map((index)=>{
-              const d = index.date.split('-')
-               const month = d[1];
-                const year = d[2];
-                if(twoDigitYear===year){
-                  //  console.log('year')
-                  if(greaterMonth<=month){
-                      // console.log('month')
-                      if(Lessmonth<=less){
-                          // console.log('index')
-                          income.push(index)
-                      }
-                  }
-                }
-           })
-             
-             
-          return res.status(200).json({code:200,message:'data fetched sucessfully',user:user,bank:bank,income:income,expences:expences})
+  
+          return res.status(200).json({code:200,message:'data fetched sucessfully',user:user,bank:bank})
   
     }catch(err){
        console.log(err.message)
@@ -78,16 +33,16 @@ exports.getUserDataByToken=async(req,res)=>{
        if(!user||!bankDetail||!token){
          throw new Error('invalid form')
        }
-       const {name,gstin,phone,email,building,city,district,state,activities,pincode} = user
+       const {name,gstin,phone,email,building,city,district,state,activities,pincode,landmark} = user
        const {Accountname,no,isfc,bankName,branch} = bankDetail
-       if(!name||!gstin||!phone||!email||!building||!city||!district||!state||!activities||!pincode||!Accountname||!no||!isfc||!bankName||!branch) 
+       if(!name||!gstin||!phone||!email||!building||!city||!district||!state||!activities||!pincode||!Accountname||!no||!isfc||!bankName||!branch||!landmark)
        {
          return res.status(200).json({code:400,message:'form not full filled'})
         }
         console.log(image)
         
           const id = await UserModel.convertToken(token);
-     const updateProfile = await profileModel.findOneAndUpdate({id:id},{$set:{name:name,gstin:gstin,phone:phone,building:building,city:city,district:district,email:email,image:image,state:state,pincode:pincode,activities:activities}})
+     const updateProfile = await profileModel.findOneAndUpdate({id:id},{$set:{name:name,gstin:gstin,phone:phone,building:building,city:city,district:district,email:email,image:image,state:state,pincode:pincode,activities:activities,landmark:landmark}})
      const updateBank = await bankModel.findOneAndUpdate({id:id},{$set:{name:Accountname,no:no,isfc:isfc,bank:bankName,branch:branch}})
       return res.status(200).json({code:200,message:'Setup Complete'})
   }catch(err){
--- src/model/IncomeModel.js
+++ src/model/IncomeModel.js
@@ -2,9 +2,8 @@ const mongoose = require('mongoose')
 
 const incomeSchema = new mongoose.Schema({
     title:{type:String,require:true},
-    category:{type:String,require:true},
+    type:{type:String,require:true},
     amount:{type:Number,require:true},
-    provisionAmt:[],
     id:{type:String,require:true},
     date:{type:String,require:true}
 },{
