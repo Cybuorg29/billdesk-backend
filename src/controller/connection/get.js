81a3aa4e91c2ba29ed1592bff79f8a3fa41a8a2b
Arc Technologies <118550452+ArctechTechnologies@users.noreply.github.com>
2024-01-02 22:35:30 +0530

    Update get.js

--- src/controller/connection/get.js
+++ src/controller/connection/get.js
@@ -7,30 +7,31 @@ const UserModel = require("../../model/userModel");
 
 
 
- async function  getData(id){
+async function getData(id) {
 
-    let   res = await profileModel.findOne({id:id});
-   
-        return res;
-    
+      let res = await profileModel.findOne({ id: id });
+
+      return res;
+
 
 }
 
 exports.getConnections = async (req, res) => {
       try {
             let data = {
-                client:[],
-                supplier:[]
+                  client: [],
+                  supplier: []
             }
-                console.log('asdasd')
+            console.log('asdasd')
             const { token } = req.params;
             const _id = await UserModel.convertToken(token);
             const Connections = await connectionModel.find({ $or: [{ cid: _id }, { sid: _id }] });
-             await  Promise.all(Connections.map(async(index)=>{
-                   (index.type)?  data.client.push(await getData(index.sid)): data.supplier.push(await getData(index.cid))
-               })
-             )
-            return res.status(200).json({ code: 200, package: data});
+            await Promise.all(Connections.map(async (index) => {
+                  (index.type) ? data.client.push(await getData(index.sid)) : data.supplier.push(await getData(index.cid))
+            })
+            )
+            console.log(data)
+            return res.status(200).json({ code: 200, package: data });
       } catch (err) {
             console.log(err.message);
             return res.status(200).json({ code: 500, message: 'an error occured', error: err.message })
