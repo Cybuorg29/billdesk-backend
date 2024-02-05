cdeaa161ad4313f9833d70e5150266dbf6735f51
Arc Technologies <118550452+ArctechTechnologies@users.noreply.github.com>
2024-02-05 23:09:00 +0530

    Update UserModel.js

--- src/model/UserModel.js
+++ src/model/UserModel.js
@@ -7,40 +7,40 @@ const jwt = require('jsonwebtoken')
 
 const userModelSchema = new mongoose.Schema({
 
-    name: { type: String },
-    phone: { type: String },
-    email: { type: String },
-    username: { type: String },
-    password: { type: String }
+  name: { type: String },
+  phone: { type: String },
+  email: { type: String },
+  username: { type: String },
+  password: { type: String }
 
 }, {
-    timestamps: true,
+  timestamps: true,
 });
 
 
 userModelSchema.statics.login = async function (username, password) {
-    const user = await this.findOne({ username });
-    console.log(user)
-    if (user) {
-        //   const auth = await bcrypt.compare(password, user.password);
-        const auth = await bcrypt.compare(password, user.password)
-        console.log(auth)
+  const user = await this.findOne({ username });
+  console.log(user)
+  if (user) {
+    //   const auth = await bcrypt.compare(password, user.password);
+    const auth = await bcrypt.compare(password, user.password)
+    console.log(auth)
 
-        if (auth) {
-            const token = jwt.sign(user._id, process.env.SECRET_KEY)
-            console.log(token)
-            return token
-        }
-        throw Error('Incorrect Password');
+    if (auth) {
+      const token = jwt.sign(user._id, process.env.SECRET_KEY)
+      console.log(token)
+      return token
     }
-    throw Error('No user found');
+    throw Error('Incorrect Password');
+  }
+  throw Error('No user found');
 };
 
 userModelSchema.statics.convertToken = async function (token) {
-    const id = jwt.verify(token, process.env.SECRET_KEY)
-    return id.id
+  const id = jwt.verify(token, process.env.SECRET_KEY)
+  return id.id
 }
 
-const USERModel = mongoose.model('accounts', userModelSchema)
+const UserModel = mongoose.model('accounts', userModelSchema)
 
-module.exports = USERModel
+module.exports = UserModel
